fff565f753c9efcbd4754d7c5aba0328944dd23c
Mr. VannyCon <ivan.contrevida4@gmail.com>
2025-02-17 21:07:39 +0800

    english version

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
-  systemInstruction: "reply in Tagalog",
+  systemInstruction: "make it more clear and concise, make it more knowledgeable in Sugar cane and don't accept other question which is not related to sugarcane or plants or insects or pest if to far from sugarcane then response to it that your question is not related to Sugarcane.",
 });
 
 const generationConfig = {
